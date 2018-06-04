4d6c56af8e2d966fc8c8707eb167853af1f10453
sawirricardo <sawir.ricardo@gmail.com>
2018-06-04 22:28:25 +0700

    initial commit

---
+++ scripts/audiokuraskali.js
@@ -0,0 +1,371 @@
+const air = new Howl({
+  src: [
+    "../assets/air.mp3",
+    "Add File",
+    "Add File"
+  ],
+  autoplay: false,
+  volume: 0.5,
+  loop: true,
+  preload: true,
+  rate: 1
+});
+
+const alert = new Howl({
+    src: [
+        "../assets/alert.mp3",
+        "Add File",
+        "Add File"
+    ],
+    autoplay: false,
+    volume: 0.5,
+    loop: false,
+    preload: true,
+    rate: 1
+});
+
+const ambient = new Howl({
+    src: [
+        "../assets/ambient.mp3",
+        "Add File",
+        "Add File"
+    ],
+    autoplay: false,
+    volume: 0.5,
+    loop: true,
+    preload: true,
+    rate: 1
+});
+
+const buzzing = new Howl({
+    src: [
+        "../assets/buzzing.mp3",
+        "Add File",
+        "Add File"
+    ],
+    autoplay: false,
+    volume: 0.5,
+    loop: false,
+    preload: true,
+    rate: 1
+});
+
+const cebur = new Howl({
+    src: [
+        "../assets/cebur.mp3",
+        "Add File",
+        "Add File"
+    ],
+    autoplay: false,
+    volume: 0.5,
+    loop: false,
+    preload: true,
+    rate: 1
+});
+
+const combat = new Howl({
+    src: [
+        "../assets/combat.mp3",
+        "Add File",
+        "Add File"
+    ],
+    autoplay: false,
+    volume: 0.5,
+    loop: false,
+    preload: true,
+    rate: 1
+});
+
+const epicchase = new Howl({
+    src: [
+        "../assets/epicchase.mp3",
+        "Add File",
+        "Add File"
+    ],
+    autoplay: false,
+    volume: 0.5,
+    loop: false,
+    preload: true,
+    rate: 1
+});
+
+const fight = new Howl({
+    src: [
+        "../assets/fight.mp3",
+        "Add File",
+        "Add File"
+    ],
+    autoplay: false,
+    volume: 0.5,
+    loop: false,
+    preload: true,
+    rate: 1
+});
+
+const hore = new Howl({
+    src: [
+        "../assets/hore.mp3",
+        "Add File",
+        "Add File"
+    ],
+    autoplay: false,
+    volume: 0.5,
+    loop: false,
+    preload: true,
+    rate: 1
+});
+
+const hutan = new Howl({
+  src: ["../assets/hutan.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: true,
+  preload: true,
+  rate: 1
+});
+
+const jedug = new Howl({
+  src: ["../assets/jedug.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: false,
+  preload: true,
+  rate: 1
+});
+
+const mainmenu = new Howl({
+  src: ["../assets/main-menu.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: true,
+  preload: true,
+  rate: 1
+});
+
+const malam = new Howl({
+  src: ["../assets/malam.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: true,
+  preload: true,
+  rate: 1
+});
+
+const masukkampung = new Howl({
+  src: ["../assets/masukkampung.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: true,
+  preload: true,
+  rate: 1
+});
+
+const over = new Howl({
+  src: ["../assets/over.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: false,
+  preload: true,
+  rate: 1
+});
+
+const panah = new Howl({
+  src: ["../assets/panah.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: false,
+  preload: true,
+  rate: 1
+});
+
+const panting = new Howl({
+  src: ["../assets/panting.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: false,
+  preload: true,
+  rate: 1
+});
+
+const pomapoma = new Howl({
+  src: ["../assets/poma-poma.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: true,
+  preload: true,
+  rate: 1
+});
+
+const romance = new Howl({
+  src: ["../assets/romance.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: true,
+  preload: true,
+  rate: 1
+});
+
+const sigh = new Howl({
+  src: ["../assets/sigh.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: false,
+  preload: true,
+  rate: 1
+});
+
+const sore = new Howl({
+  src: ["../assets/sore.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: false,
+  preload: true,
+  rate: 1
+});
+
+const suaralesung = new Howl({
+  src: ["../assets/suaralesung.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.1,
+  loop: false,
+  preload: true,
+  rate: 1
+});
+
+const sungai = new Howl({
+  src: ["../assets/sungai.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: true,
+  preload: true,
+  rate: 1
+});
+
+const underwater = new Howl({
+  src: ["../assets/underwater.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: true,
+  preload: true,
+  rate: 1
+});
+
+const monyet = new Howl({
+  src: ["../assets/monyet_1.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: true,
+  preload: true,
+  rate: 1
+});
+
+
+const myAir = document.getElementById("u39774");
+const myAlert = document.getElementById("pu25630");
+const myAmbient = document.getElementById("u39968");
+const myBuzzing = document.getElementById("u40004");
+const myCebur = document.getElementById("u25662-4");
+const myCombat = document.getElementById("u39822");
+const myEpicchase = document.getElementById("pu25630");
+const myFight = document.getElementById("pu25630");
+const myHore = document.getElementById("u39798");
+const myHutan = document.getElementById("u39644");
+const myJedug = document.getElementById("u40566");
+const myJedug2 = document.getElementById("u40648");
+const myMainMenu = document.getElementById("pu25630");
+const myMalam = document.getElementById("pu25630");
+const myMasukkampung = document.getElementById("u39750");
+const myOver = document.getElementById("pu25630");
+const myPanah = document.getElementById("u40704");
+const myPanting = document.getElementById("pu25630");
+const myPomapoma = document.getElementById("u39956");
+const myRomance = document.getElementById("pu25630");
+const mySigh = document.getElementById("pu25630");
+const mySore = document.getElementById("pu25630");
+const mySuaralesung = document.getElementById("u40660");
+const mySungai = document.getElementById("pu25630");
+const myUnderwater = document.getElementById("pu25630");
+const myMonyet = document.getElementById("u44747");
+const myMonyet2 = document.getElementById("u44859");
+
+const elementAir = scrollMonitor.create(myAir, { top: 0, bottom: 0 });
+const elementAlert = scrollMonitor.create(myAlert, { top: 0, bottom: 0 });
+const elementAmbient = scrollMonitor.create(myAmbient, { top: 0, bottom: 0 });
+const elementBuzzing = scrollMonitor.create(myBuzzing, { top: 0, bottom: 0 });
+const elementCebur = scrollMonitor.create(myCebur, { top: 0, bottom: 0 });
+const elementCombat = scrollMonitor.create(myCombat, { top: 0, bottom: 0 });
+const elementEpicchase = scrollMonitor.create(myEpicchase, {top:0, bottom: 0});
+const elementFight = scrollMonitor.create(myFight, { top: 0, bottom: 0 });
+const elementHore = scrollMonitor.create(myHore, { top: 0, bottom: 0 });
+const elementHutan = scrollMonitor.create(myHutan, { top: 0, bottom: 0 });
+const elementJedug = scrollMonitor.create(myJedug, { top: 0, bottom: 0 });
+const elementJedug2 = scrollMonitor.create(myJedug2, { top: 0, bottom: 0 });
+const elementMalam = scrollMonitor.create(myMalam, { top: 0, bottom: 0 });
+const elementMasukkampung = scrollMonitor.create(myMasukkampung, {top:0, bottom: 0});
+const elementOver = scrollMonitor.create(myOver, { top: 0, bottom: 0 });
+const elementPanah = scrollMonitor.create(myPanah, { top: 0, bottom: 0 });
+const elementPanting = scrollMonitor.create(myPanting, { top: 0, bottom: 0 });
+const elementPomapoma = scrollMonitor.create(myPomapoma, { top: 0, bottom: 0 });
+const elementRomance = scrollMonitor.create(myRomance, { top: 0, bottom: 0 });
+const elementSigh = scrollMonitor.create(mySigh, { top: 0, bottom: 0 });
+const elementSore = scrollMonitor.create(mySore, { top: 0, bottom: 0 });
+const elementSuaralesung = scrollMonitor.create(mySuaralesung, {top:0, bottom: 0});
+const elementSungai = scrollMonitor.create(mySungai, { top: 0, bottom: 0 });
+const elementUnderwater = scrollMonitor.create(myUnderwater, {top:0, bottom: 0});
+const elementMonyet = scrollMonitor.create(myMonyet, { top: 0, bottom: 0 });
+const elementMonyet2 = scrollMonitor.create(myMonyet2, { top: 0, bottom: 0 });
+
+elementAir.enterViewport(() => air.play())
+// elementAlert.enterViewport(() => alert.play())
+elementAmbient.enterViewport(() => ambient.play())
+elementBuzzing.enterViewport(() => buzzing.play())
+elementCebur.enterViewport(() => cebur.play())
+elementCombat.enterViewport(() => combat.play())
+// elementEpicchase.enterViewport(() => epicchase.play())
+// elementFight.enterViewport(() => fight.play())
+elementHore.enterViewport(() => hore.play())
+elementHutan.enterViewport(() => hutan.play())
+elementJedug.enterViewport(() => jedug.play())
+elementJedug2.enterViewport(() => jedug.play())
+// elementMalam.enterViewport(() => malam.play())
+elementMasukkampung.enterViewport(() => masukkampung.play())
+// elementOver.enterViewport(() => over.play())
+elementPanah.enterViewport(() => panah.play())
+// elementPanting.enterViewport(() => panting.play())
+elementPomapoma.enterViewport(() => pomapoma.play())
+// elementRomance.enterViewport(() => romance.play())
+// elementSigh.enterViewport(() => sigh.play())
+// elementSore.enterViewport(() => sore.play())
+elementSuaralesung.enterViewport(() => suaralesung.play())
+// elementSungai.enterViewport(() => sungai.play())
+// elementUnderwater.enterViewport(() => underwater.play())
+elementMonyet.enterViewport(() => monyet.play())
+elementMonyet2.enterViewport(() => monyet.play());
+
+
+elementAir.exitViewport(() => air.stop());
+//elementAlert.exitViewport(() => alert.stop());
+elementAmbient.exitViewport(() => ambient.stop());
+elementBuzzing.exitViewport(() => buzzing.stop());
+elementCebur.exitViewport(() => cebur.stop());
+elementCombat.exitViewport(() => combat.stop());
+//elementEpicchase.exitViewport(() => epicchase.stop());
+//elementFight.exitViewport(() => fight.stop());
+elementHore.exitViewport(() => hore.stop());
+elementHutan.exitViewport(() => hutan.stop());
+elementJedug.exitViewport(() => jedug.stop());
+elementJedug2.exitViewport(() => jedug.stop());
+//elementMalam.exitViewport(() => malam.stop());
+elementMasukkampung.exitViewport(() => masukkampung.stop());
+//elementOver.exitViewport(() => over.stop());
+elementPanah.exitViewport(() => panah.stop());
+elementPanting.exitViewport(() => panting.stop());
+elementPomapoma.exitViewport(() => pomapoma.stop());
+//elementRomance.exitViewport(() => romance.stop());
+//elementSigh.exitViewport(() => sigh.stop());
+//elementSore.exitViewport(() => sore.stop());
+elementSuaralesung.exitViewport(() => suaralesung.stop());
+//elementSungai.exitViewport(() => sungai.stop());
+//elementUnderwater.exitViewport(() => underwater.stop());
+elementMonyet.exitViewport(() => monyet.stop());
+elementMonyet2.exitViewport(() => monyet.stop());
---
+++ scripts/audiomenggagalkan.js
@@ -0,0 +1,390 @@
+const air = new Howl({
+  src: [
+    "../assets/air.mp3",
+    "Add File",
+    "Add File"
+  ],
+  autoplay: false,
+  volume: 0.5,
+  loop: true,
+  preload: true,
+  rate: 1
+});
+
+const alert = new Howl({
+    src: [
+        "../assets/alert.mp3",
+        "Add File",
+        "Add File"
+    ],
+    autoplay: false,
+    volume: 0.5,
+    loop: false,
+    preload: true,
+    rate: 1
+});
+
+const ambient = new Howl({
+    src: [
+        "../assets/ambient.mp3",
+        "Add File",
+        "Add File"
+    ],
+    autoplay: false,
+    volume: 0.5,
+    loop: true,
+    preload: true,
+    rate: 1
+});
+
+const buzzing = new Howl({
+    src: [
+        "../assets/buzzing.mp3",
+        "Add File",
+        "Add File"
+    ],
+    autoplay: false,
+    volume: 0.5,
+    loop: false,
+    preload: true,
+    rate: 1
+});
+
+const cebur = new Howl({
+    src: [
+        "../assets/cebur.mp3",
+        "Add File",
+        "Add File"
+    ],
+    autoplay: false,
+    volume: 0.5,
+    loop: false,
+    preload: true,
+    rate: 1
+});
+
+const combat = new Howl({
+    src: [
+        "../assets/combat.mp3",
+        "Add File",
+        "Add File"
+    ],
+    autoplay: false,
+    volume: 0.5,
+    loop: false,
+    preload: true,
+    rate: 1
+});
+
+const epicchase = new Howl({
+    src: [
+        "../assets/epicchase.mp3",
+        "Add File",
+        "Add File"
+    ],
+    autoplay: false,
+    volume: 0.5,
+    loop: false,
+    preload: true,
+    rate: 1
+});
+
+const fight = new Howl({
+    src: [
+        "../assets/fight.mp3",
+        "Add File",
+        "Add File"
+    ],
+    autoplay: false,
+    volume: 0.5,
+    loop: false,
+    preload: true,
+    rate: 1
+});
+
+const hore = new Howl({
+    src: [
+        "../assets/hore.mp3",
+        "Add File",
+        "Add File"
+    ],
+    autoplay: false,
+    volume: 0.5,
+    loop: false,
+    preload: true,
+    rate: 1
+});
+
+const hutan = new Howl({
+  src: ["../assets/hutan.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: true,
+  preload: true,
+  rate: 1
+});
+
+const jedug = new Howl({
+  src: ["../assets/jedug.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: false,
+  preload: true,
+  rate: 1
+});
+
+const mainmenu = new Howl({
+  src: ["../assets/main-menu.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: true,
+  preload: true,
+  rate: 1
+});
+
+const malam = new Howl({
+  src: ["../assets/malam.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: true,
+  preload: true,
+  rate: 1
+});
+
+const masukkampung = new Howl({
+  src: ["../assets/masukkampung.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: true,
+  preload: true,
+  rate: 1
+});
+
+const over = new Howl({
+  src: ["../assets/over.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: false,
+  preload: true,
+  rate: 1
+});
+
+const panah = new Howl({
+  src: ["../assets/panah.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: false,
+  preload: true,
+  rate: 1
+});
+
+const panting = new Howl({
+  src: ["../assets/panting.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: false,
+  preload: true,
+  rate: 1
+});
+
+const pomapoma = new Howl({
+  src: ["../assets/poma-poma.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: true,
+  preload: true,
+  rate: 1
+});
+
+const romance = new Howl({
+  src: ["../assets/romance.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: true,
+  preload: true,
+  rate: 1
+});
+
+const sigh = new Howl({
+  src: ["../assets/sigh.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: false,
+  preload: true,
+  rate: 1
+});
+
+const sore = new Howl({
+  src: ["../assets/sore.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: false,
+  preload: true,
+  rate: 1
+});
+
+const suaralesung = new Howl({
+  src: ["../assets/suaralesung.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.1,
+  loop: false,
+  preload: true,
+  rate: 1
+});
+
+const sungai = new Howl({
+  src: ["../assets/sungai.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: true,
+  preload: true,
+  rate: 1
+});
+
+const underwater = new Howl({
+  src: ["../assets/underwater.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: true,
+  preload: true,
+  rate: 1
+});
+
+const monyet = new Howl({
+  src: ["../assets/monyetteriak.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: true,
+  preload: true,
+  rate: 1
+});
+
+const riot = new Howl({
+  src: ["../assets/riot.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: false,
+  preload: true,
+  rate: 1
+});
+
+const waterfall = new Howl({
+  src: ["../assets/waterfall.mp3", "Add File", "Add File"],
+  autoplay: false,
+  volume: 0.5,
+  loop: true,
+  preload: true,
+  rate: 1
+});
+
+
+const myAir = document.getElementById("u44641");
+const myAlert = document.getElementById("u40796");
+const myAmbient = document.getElementById("page");
+const myBuzzing = document.getElementById("page");
+const myCebur = document.getElementById("u40826");
+const myCombat = document.getElementById("u40769");
+const myEpicchase = document.getElementById("u40778");
+const myFight = document.getElementById("page");
+const myHore = document.getElementById("page");
+const myHutan = document.getElementById("page");
+const myJedug = document.getElementById("u40784");
+const myMainMenu = document.getElementById("page");
+const myMalam = document.getElementById("page");
+const myMasukkampung = document.getElementById("page");
+const myOver = document.getElementById("page");
+const myPanah = document.getElementById("u40844");
+const myPanting = document.getElementById("u40853");
+const myPomapoma = document.getElementById("page");
+const myRomance = document.getElementById("page");
+const mySigh = document.getElementById("page");
+const mySore = document.getElementById("page");
+const mySuaralesung = document.getElementById("page");
+const mySungai = document.getElementById("page");
+const myUnderwater = document.getElementById("u40847");
+const myMonyet = document.getElementById("u31115-4");
+const myRiot = document.getElementById("u40802");
+const myRiot2 = document.getElementById("u40808");
+const myWaterfall = document.getElementById("u44641");
+
+const elementAir = scrollMonitor.create(myAir, { top: 0, bottom: 0 });
+const elementAlert = scrollMonitor.create(myAlert, { top: 0, bottom: 0 });
+const elementAmbient = scrollMonitor.create(myAmbient, { top: 0, bottom: 0 });
+const elementBuzzing = scrollMonitor.create(myBuzzing, { top: 0, bottom: 0 });
+const elementCebur = scrollMonitor.create(myCebur, { top: 0, bottom: 0 });
+const elementCombat = scrollMonitor.create(myCombat, { top: 0, bottom: 0 });
+const elementEpicchase = scrollMonitor.create(myEpicchase, {top:0, bottom: 0});
+const elementFight = scrollMonitor.create(myFight, { top: 0, bottom: 0 });
+const elementHore = scrollMonitor.create(myHore, { top: 0, bottom: 0 });
+const elementHutan = scrollMonitor.create(myHutan, { top: 0, bottom: 0 });
+const elementJedug = scrollMonitor.create(myJedug, { top: 0, bottom: 0 });
+const elementMalam = scrollMonitor.create(myMalam, { top: 0, bottom: 0 });
+const elementMasukkampung = scrollMonitor.create(myMasukkampung, {top:0, bottom: 0});
+const elementOver = scrollMonitor.create(myOver, { top: 0, bottom: 0 });
+const elementPanah = scrollMonitor.create(myPanah, { top: 0, bottom: 0 });
+const elementPanting = scrollMonitor.create(myPanting, { top: 0, bottom: 0 });
+const elementPomapoma = scrollMonitor.create(myPomapoma, { top: 0, bottom: 0 });
+const elementRomance = scrollMonitor.create(myRomance, { top: 0, bottom: 0 });
+const elementSigh = scrollMonitor.create(mySigh, { top: 0, bottom: 0 });
+const elementSore = scrollMonitor.create(mySore, { top: 0, bottom: 0 });
+const elementSuaralesung = scrollMonitor.create(mySuaralesung, {top:0, bottom: 0});
+const elementSungai = scrollMonitor.create(mySungai, { top: 0, bottom: 0 });
+const elementUnderwater = scrollMonitor.create(myUnderwater, {top:0, bottom: 0});
+const elementMonyet = scrollMonitor.create(myMonyet, { top: 0, bottom: 0 });
+const elementRiot = scrollMonitor.create(myRiot, { top: 0, bottom: 0 });
+
+const elementWaterfall = scrollMonitor.create(myWaterfall, { top: 0, bottom: 0 });
+
+// elementAir.enterViewport(() => air.play())
+elementAlert.enterViewport(() => alert.play())
+// elementAmbient.enterViewport(() => ambient.play())
+// elementBuzzing.enterViewport(() => buzzing.play())
+elementCebur.enterViewport(() => cebur.play())
+elementCombat.enterViewport(() => combat.play())
+elementEpicchase.enterViewport(() => epicchase.play())
+// elementFight.enterViewport(() => fight.play())
+// elementHore.enterViewport(() => hore.play())
+// elementHutan.enterViewport(() => hutan.play())
+elementJedug.enterViewport(() => jedug.play())
+// elementMalam.enterViewport(() => malam.play())
+// elementMasukkampung.enterViewport(() => masukkampung.play())
+// elementOver.enterViewport(() => over.play())
+elementPanah.enterViewport(() => panah.play())
+elementPanting.enterViewport(() => panting.play())
+// elementPomapoma.enterViewport(() => pomapoma.play())
+// elementRomance.enterViewport(() => romance.play())
+// elementSigh.enterViewport(() => sigh.play())
+// elementSore.enterViewport(() => sore.play())
+// elementSuaralesung.enterViewport(() => suaralesung.play())
+// elementSungai.enterViewport(() => sungai.play())
+elementUnderwater.enterViewport(() => underwater.play())
+elementMonyet.enterViewport(() => monyet.play())
+elementRiot.enterViewport(() => riot.play())
+elementWaterfall.enterViewport(() => waterfall.play())
+
+// elementAir.exitViewport(() => air.stop());
+//elementAlert.exitViewport(() => alert.stop());
+// elementAmbient.exitViewport(() => ambient.stop());
+// elementBuzzing.exitViewport(() => buzzing.stop());
+elementCebur.exitViewport(() => cebur.stop());
+elementCombat.exitViewport(() => combat.stop());
+elementEpicchase.exitViewport(() => epicchase.stop());
+//elementFight.exitViewport(() => fight.stop());
+//elementHore.exitViewport(() => hore.stop());
+//elementHutan.exitViewport(() => hutan.stop());
+elementJedug.exitViewport(() => jedug.stop());
+//elementMalam.exitViewport(() => malam.stop());
+//elementMasukkampung.exitViewport(() => masukkampung.stop());
+//elementOver.exitViewport(() => over.stop());
+elementPanah.exitViewport(() => panah.stop());
+elementPanting.exitViewport(() => panting.stop());
+// elementPomapoma.exitViewport(() => pomapoma.stop());
+//elementRomance.exitViewport(() => romance.stop());
+//elementSigh.exitViewport(() => sigh.stop());
+//elementSore.exitViewport(() => sore.stop());
+// elementSuaralesung.exitViewport(() => suaralesung.stop());
+//elementSungai.exitViewport(() => sungai.stop());
+elementUnderwater.exitViewport(() => underwater.stop());
+elementMonyet.exitViewport(() => monyet.stop());
+elementRiot.exitViewport(() => riot.stop());
+elementWaterfall.exitViewport(() => waterfall.stop());
